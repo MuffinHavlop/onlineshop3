c0ef80204d3ac6cabf1627062bb9269cfba25a34
muffinhavlop <101418643+MuffinHavlop@users.noreply.github.com>
2023-05-01 15:23:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+let user;
+
+document.getElementById("button1").onclick = function(){
+    user = document.getElementById("button1").value;
+    document.getElementById("button1").innerHTML = "Added to cart";
+}
+document.getElementById("button2").onclick = function(){
+    user = document.getElementById("button2").value;
+    document.getElementById("button2").innerHTML = "added to cart";
+}
+document.getElementById("button3").onclick = function(){
+    user = document.getElementById("button3").value;
+    document.getElementById("button3").innerHTML = "added to cart";
+}
+document.getElementById("button4").onclick = function(){
+    user = document.getElementById("button4").value;
+    document.getElementById("button4").innerHTML = "added to cart";
+}
+document.getElementById("button5").onclick = function(){
+    user = document.getElementById("button5").value;
+    document.getElementById("button5").innerHTML = "added to cart";
+}
+document.getElementById("button6").onclick = function(){
+    user = document.getElementById("button6").value;
+    document.getElementById("button6").innerHTML = "added to cart";
+}
+document.getElementById("button7").onclick = function(){
+    user = document.getElementById("button7").value;
+    document.getElementById("button7").innerHTML = "you get no beaches";
+}
+document.getElementById("button8").onclick = function(){
+    user = document.getElementById("button8").value;
+    document.getElementById("button8").innerHTML = "you get no beaches";
+}
+document.getElementById("button9").onclick = function(){
+    user = document.getElementById("button9").value;
+    document.getElementById("button9").innerHTML = "you get no beaches";
+}
+document.getElementById("button10").onclick = function(){
+    user = document.getElementById("button10").value;
+    document.getElementById("button10").innerHTML = "you get no beaches";
+}
+document.getElementById("button11").onclick = function(){
+    user = document.getElementById("button11").value;
+    document.getElementById("button11").innerHTML = "you get no beaches";
+}
+document.getElementById("button12").onclick = function(){
+    user = document.getElementById("button12").value;
+    document.getElementById("button12").innerHTML = "Out of stock";
+}
+
